68ef280bbcb2a96b4c8c46e247c709e9a5943275
LeviS <levisporto14@gmail.com>
2025-12-04 21:55:41 -0300

    tailwind added

---
+++ src/Components/Header.jsx
@@ -0,0 +1,35 @@
+
+import { useState } from 'react'
+
+
+function Header() {
+ 
+  return (
+    <>
+      <header>
+<img src="/digital.svg" alt="React Logo" />
+<p>
+    Digital Store
+</p>
+ <input type="text" placeholder="Pesquisar produto.."></input>
+<p>Cadastre-se</p>
+ <button type="button">Entrar</button> 
+ <p>Home</p>
+ <p>Produtos</p>
+ <p>Categorias</p>
+ <p>Meus pedidos</p>
+      </header>
+      
+
+
+
+
+
+      
+    </>
+  )
+}
+
+export default Header
+
+
