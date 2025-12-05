2d5d072301dafada5bdaac97c23995716b1a248c
Levi S. Porto <62207875+levisporto@users.noreply.github.com>
2025-12-05 08:46:20 -0300

    iniciando header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,19 +6,42 @@ function Header() {
  
   return (
     <>
-      <header>
-<img src="/digital.svg" alt="React Logo" />
-<p>
+    <div class="grid ">
+    <div class="col-span-5"><h1><header class="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 justify-between">
+        <div  class="flex  gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4">
+        <img src="/digital.svg" alt="Ícone da Digital" />
+<p class="text-9x1">
     Digital Store
 </p>
- <input type="text" placeholder="Pesquisar produto.."></input>
+<div class="flex px-4 py-3 rounded-md border border-white bg-gray-100 border-white-500 overflow-hidden max-w-md mx-auto">
+        <input placeholder="Pesquisar produto..."
+          class="w-full outline-none bg-transparent text-gray-600 text-sm" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192.904 192.904" width="16px" class="fill-gray-600">
+          <path
+            d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z">
+          </path>
+        </svg>
+      </div>
 <p>Cadastre-se</p>
- <button type="button">Entrar</button> 
- <p>Home</p>
+
+ 
+ 
+        </div>
+        <div  class="flex flex-col gap-2 p-8 sm:flex-row   self-end">
+        <button type="button" class="bg-pink-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >Entrar</button> 
+        <img src="/carrinho.png" alt="Ícone do Carrinho" />
+        </div>
+      </header></h1></div>
+    <div class="col-span-5 row-start-2"> 
+      <div class="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ">
+      <p>Home</p>
  <p>Produtos</p>
  <p>Categorias</p>
  <p>Meus pedidos</p>
-      </header>
+      </div>
+ </div>
+</div>
+      
       
 
 
